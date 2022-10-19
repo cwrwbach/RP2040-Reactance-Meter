45b49c642ae620139873ef7e2efb23e9ab326ce3
Dai <dai@here.com>
2022-10-19 11:54:03 +0000

    Buttons added. Separte Calib and Measure subs. Basic calib-measure sequencs now seems to work well

--- freq_counter.c
+++ freq_counter.c
@@ -7,6 +7,11 @@
 
 const uint MEASURE_PIN = 15; //This is GPIO 15 = PIN 20 = Slice 7B
 const uint LED_PIN = 13;
+const uint LEFT = 27;
+const uint RIGHT = 22;
+const uint TOP = 28;
+const uint BOT = 26;
+
 uint slice_num;
 pwm_config cfg;
 
@@ -15,11 +20,13 @@ int lcd_startup();
 void lcd_set_cursor(int,int);
 void lcd_string(const char *s);
 
+//globals
+double f1,c1,l1;
+
 //----
 
 int measure_duty_cycle() 
 {
-
 pwm_init(slice_num, &cfg, false);
 sleep_ms(10); 
 pwm_set_enabled(slice_num, true);
@@ -27,48 +34,70 @@ sleep_ms(1000);
 pwm_set_enabled(slice_num, false);
 
 unsigned int val = pwm_get_counter(slice_num);
-//printf(" Count = %d \n",val*20);
 
 itoa(val*2,i_val,10);
 lcd_set_cursor(0,0);
 lcd_string(i_val);
-
-
 return val*2;
 }
 
-//---
-
-
 relay_on()
 {
-
-
 gpio_init(LED_PIN);
 gpio_set_dir(LED_PIN, GPIO_OUT);
-//while (1) 
-    {
-//   gpio_put(LED_PIN, 0);
-//  sleep_us(1);
-    gpio_put(LED_PIN, 1);
-   sleep_ms(2);
-    }
+gpio_put(LED_PIN, 1);
+sleep_ms(2);
 }
 relay_off()
 {
 
 gpio_init(LED_PIN);
 gpio_set_dir(LED_PIN, GPIO_OUT);
-//while (1) 
-    {
-//   gpio_put(LED_PIN, 0);
-//  sleep_us(1);
-    gpio_put(LED_PIN, 0);
-   sleep_ms(2);
-    }
+gpio_put(LED_PIN, 0);
+sleep_ms(2);
+
 }
 
+do_calib()
+{
+uint32_t freq;
+//double c1;
+double f2;
+printf("\nStarting Calib \n");
+relay_off();
+freq = measure_duty_cycle();
+sleep_ms(1000);
+f1=(double) freq;
+f1 = f1 * 20;
 
+relay_on();
+freq = measure_duty_cycle();
+
+sleep_ms(1000);    
+f2=(double) freq;
+f2 = f2 * 20;
+
+printf(" f1: %f f2: %f \n",f1,f2); 
+c1= ((f2*f2) / ((f1*f1) - (f2*f2))) * 1000e-12;
+printf(" C1: %6.3f pF \n",c1 * 1e12);
+}
+
+do_measure()
+{
+uint32_t freq;
+double cx;
+double f2;
+printf("\nStarting Measure \n");
+relay_off();
+freq = measure_duty_cycle();
+sleep_ms(1000);
+f2=(double) freq;
+f2 = f2 * 20;
+
+printf(" f1: %f f2: %f \n",f1,f2); 
+cx= ( ((f1*f1)/(f2*f2)) -1) * c1;
+printf(" CX: %6.3f pF \n",cx * 1e12);
+}
 
 //---
 
@@ -76,25 +105,31 @@ int main()
 {
 uint64_t test_val;
 uint gpio;
-
+uint test;
 char msg[16];
-uint32_t freq;
 
-double f1,f2,c1,l1;
+gpio_init(TOP);
+gpio_set_dir(TOP, GPIO_IN);
+gpio_pull_up(TOP) ; 
 
+gpio_init(BOT);
+gpio_set_dir(BOT, GPIO_IN);
+gpio_pull_up(BOT) ; 
+
+gpio_init(LEFT);
+gpio_set_dir(LEFT, GPIO_IN);
+gpio_pull_up(LEFT) ; 
+
+gpio_init(RIGHT);
+gpio_set_dir(RIGHT, GPIO_IN);
+gpio_pull_up(RIGHT) ; 
 
 gpio = MEASURE_PIN;
 
 stdio_init_all();
-printf("\nFrequency counter V0.0.5-india \n");
+printf("\nFrequency counter V0.0.6-juliet \n");
 
 lcd_startup();
-//sleep_ms(20000);
-//lcd_set_cursor(0,0);
-
-//strcpy(msg,"her again");
-
-//lcd_string(msg);
 
 sleep_ms(200);
 //  lcd_clear();
@@ -111,41 +146,23 @@ pwm_init(slice_num, &cfg, false);
 gpio_set_function(gpio, GPIO_FUNC_PWM);
 
 
-
-
-//for(int i = 0; i<100;i++)
+while(1)
     {
-relay_off();
-freq = measure_duty_cycle();
-printf("\n F1: %d\n",freq);
-sleep_ms(1000);
-f1=(double) freq;
-f1 = f1 * 20;
+    if(gpio_get(TOP) ==0)
+        {
+        do_calib();
+        printf("Done calib\n");
+        }
 
-relay_on();
-freq = measure_duty_cycle();
-printf("\n F2: %d\n",freq);
+    if(gpio_get(BOT) ==0)
+        {
+        do_measure();
+        printf(" Done measure \n");
+        }
 
-sleep_ms(1000);    
-f2=(double) freq;
-f2 = f2 * 20;
-
-printf(" f1: %f f2: %f \n",f1,f2); 
-//f1=738e3;
-//f2=477e3;
-
-c1= ((f2*f2) / ((f1*f1) - (f2*f2))) * 1000e-12;
-
-printf(" C1: %6.3f pF \n",c1 * 1e12);
-
-
-
-
+    if(gpio_get(LEFT) ==0)
+        printf("Left pressed \n ");
+    if(gpio_get(RIGHT) ==0)
+        printf("Right pressed \n");
     }
-
-
-
-
-
-
 }
